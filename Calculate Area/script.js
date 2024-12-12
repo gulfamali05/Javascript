8f83038da1861df64363fdc260f375a395a1e42d
Gulfam Ali <its.gulfam.a@gmail.com>
2024-12-12 10:54:16 +0500

    changes done

--- Calculate Area/script.js
+++ Calculate Area/script.js
@@ -1,14 +1,14 @@
 
 const onSubmit =()=>{
     event.preventDefault()
-    let input1 =document.getElementById("width").value
-    let input2 =document.getElementById("height").value
+    let input1 =document.querySelector("#height").value
+    let input2 =document.querySelector("#width").value
     if(input1=="" && input2==""){
         alert("invalid input")
     }
     else{
         let area = input1*input2
-        document.getElementById("areas").innerHTML=area
+        document.querySelector("#areas").innerHTML=area
     }
 
 }
