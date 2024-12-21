fcdf79bc60f04067136ee716bd77586499ed8008
gulfamali05 <its.gulfam.a@gmail.com>
2024-12-21 20:46:30 +0500

    changes done

---
+++ Day1/first.js
@@ -0,0 +1,18 @@
+// object
+const product={
+    title:"Ball Point",
+    rating:4,
+    price:270,
+    offer:"5% off",
+};
+// display object
+console.log(product);
+// display value of an key
+console.log(product["offer"]);
+// 2nd way
+console.log(product.offer);
+// upate values
+product.offer="50% off";
+console.log(product.offer);
+// check datatype
+console.log(typeof product.offer);
